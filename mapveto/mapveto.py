11235aea8d30a91ea030647c925cd290ec156948
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2024-07-23 15:13:10 +0200

    Update mapveto.py

--- mapveto/mapveto.py
+++ mapveto/mapveto.py
@@ -134,7 +134,7 @@ async def send_ticket_message(bot, veto, channel):
         components.append(MapButton(label="Attaque", veto_name=veto.name, action_type="side", channel=channel))
         components.append(MapButton(label="Défense", veto_name=veto.name, action_type="side", channel=channel))
     else:
-        for map_name in veto.listmaps:
+        for map_name in veto.maps:
             button = MapButton(label=map_name, veto_name=veto.name, action_type=action.lower(), channel=channel)
             if map_name in veto.banned_maps or map_name in veto.picked_maps:
                 button.disabled = True
@@ -280,12 +280,12 @@ class MapVeto:
 
     def ban_map(self, map_name):
         if map_name in self.maps:
-            self.maps.remove(map_name)
+            #self.maps.remove(map_name)
             self.banned_maps.append(map_name)
 
     def pick_map(self, map_name, chooser):
         if map_name in self.maps:
-            self.maps.remove(map_name)
+            #self.maps.remove(map_name)
             self.picked_maps.append({"map": map_name, "chooser": chooser})
 
     def pick_side(self, side, chooser):
