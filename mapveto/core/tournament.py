e2242caa780cf4d183fb48e73116d69d45cdd54b
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2024-07-26 22:13:25 +0200

    Update tournament.py

--- mapveto/core/tournament.py
+++ mapveto/core/tournament.py
@@ -4,6 +4,8 @@ import discord
 from discord.ui import Modal, TextInput, Button, Select, View
 from discord.ext import commands
 
+from .templateveto import veto_config
+
 class TournamentConfig:
     def __init__(self, filename="tourney.json"):
         self.filename = filename
