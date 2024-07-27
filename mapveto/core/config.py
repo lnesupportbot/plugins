d82e5391ae15cb910473a9302b9d1b0b7ab041dd
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2024-07-27 12:27:42 +0200

    Delete mapveto/core/config.py

--- mapveto/core/config.py
+++
@@ -1,6 +0,0 @@
-from .templateveto import MapVetoConfig
-from .tournament import TournamentConfig
-
-# Initialisation globale des configurations
-tournament_config = TournamentConfig()
-veto_config = MapVetoConfig()
