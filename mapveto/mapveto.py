d0f47a3f9c2fb4748a24ea82bb9a52227b66f5be
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2024-07-27 17:11:43 +0200

    Update mapveto.py

--- mapveto/mapveto.py
+++ mapveto/mapveto.py
@@ -10,7 +10,7 @@ from core.models import PermissionLevel  # type: ignore
 from .core.templateveto import MapVetoConfig, TemplateManager, vetos
 from .core.tournament import TournamentManager
 from .core.teams import TeamManager
-from .core.veto import MapVeto,send_ticket_message
+from .core.veto import MapVeto
 
 veto_config = MapVetoConfig()
 vetos = {}
