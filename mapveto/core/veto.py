cb4e4d3d462f1195acfbfb5eeecc635bd2ea906b
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2024-08-04 16:41:58 +0200

    Update veto.py

--- mapveto/core/veto.py
+++ mapveto/core/veto.py
@@ -10,7 +10,7 @@ from .templateveto import MapVetoConfig, TemplateManager, veto_config
 from .tournament import TournamentManager, TournamentConfig, tournament_config
 from .teams import TeamManager, TeamConfig, team_config
 
-from modmail import Modmail
+from cogs import modmail
 
 # Charger les configurations
 veto_config = MapVetoConfig()
@@ -19,7 +19,7 @@ tournament_config = TournamentConfig()
 tournaments = tournament_config.load_tournaments()
 team_config = TeamConfig()
 teams = team_config.load_teams()
-modmail = Modmail()
+modmail_cog = modmail.Modmail()
 
 class MapVeto:
     def __init__(self, name, maps, team_a_id, team_a_name, team_b_id, team_b_name, rules, channel, bot):
@@ -578,7 +578,7 @@ class CloseMapVetoButton(Button):
 
         # Fermer le ticket de manière silencieuse
         # Assurez-vous que le bot a la permission de supprimer le channel
-        await modmail.close(        
+        await modmail_cog.close(        
             self,
             option = "silent",
             UserFriendlyTime = None
