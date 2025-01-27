5a4be33c64217aab495e7a2276f1f760402edeea
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2025-01-27 19:08:42 +0100

    Update ListenWebhook.py

--- ListenWebhook/ListenWebhook.py
+++ ListenWebhook/ListenWebhook.py
@@ -27,9 +27,9 @@ class ListenWebhook:
         with open(self.filename, "w") as file:
             json.dump(self.webhooks, file, indent=4)
 
-    def create_lstwebhook(self, name):
+    def create_lstwebhook(self, webhook_name):
         if name not in self.webhooks:
-            self.webhooks[name]
+            self.webhooks[webhook_name]
             self.save_webhooks()
             return True
         return False
