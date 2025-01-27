b914ede7f5df0e9da56622fc066d9da8ba200ffc
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2025-01-27 18:30:55 +0100

    Update ListenWebhook.py

--- ListenWebhook/ListenWebhook.py
+++ ListenWebhook/ListenWebhook.py
@@ -56,7 +56,7 @@ class listenWebhookCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def lstweb_remove(self, ctx, webhook_name: str):
         """Supprime un webhook de la liste."""
-        if webhook_name in self.webhooks:
+        if webhook_name in webhooks_file.webhooks:
             del self.webhooks[webhook_name]
             self.save_webhooks()
             await ctx.send(f"❌ Webhook `{webhook_name}` supprimé avec succès !")
@@ -66,7 +66,7 @@ class listenWebhookCog(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         """Gère les messages provenant de webhooks enregistrés."""
-        if message.author.bot and message.author.name in self.webhooks:
+        if message.author.bot and message.author.name in webhooks_file.webhooks:
             if "purge" in message.content.lower():
                 try:
                     if message.content.split()[-1].isdigit():
