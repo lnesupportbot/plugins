9976b55a02bb3105fb13465e7ba79b1c90ee4a42
lnesupportbot <116952155+lnesupportbot@users.noreply.github.com>
2025-01-28 12:07:19 +0100

    Update ListenWebhook.py

--- ListenWebhook/ListenWebhook.py
+++ ListenWebhook/ListenWebhook.py
@@ -92,7 +92,7 @@ class listenWebhookCog(commands.Cog):
                 # Log du message reçu
                 print(f"Message reçu du webhook : ID = {webhook_id}, Nom = {webhook_name}, Contenu = {message.content}")
 
-                if message.content.startswith("!"):
+                if message.content.startswith("?"):
                     parts = message.content.split()
                     command = parts[0][1:]  # Enlève le "!"
                     args = parts[1:]
